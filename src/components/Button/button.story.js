ff86487062f0a259d92ff3f91929683fd1a5b0c4
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2017-03-16 00:08:55 -0700

    [UPDATE] update button story with code sample

--- src/components/Button/button.story.js
+++ src/components/Button/button.story.js
@@ -5,7 +5,7 @@ import Button from './Button';
 const PageDecorator = (story) => (
   <div style={{
     padding: '10px 20px',
-    textAlign: 'center'
+    textAlign: 'left'
   }}>
     { story() }
   </div>
@@ -25,16 +25,20 @@ storiesOf('Button', module)
   .add('Button types', () => (
     <ul style={listStyles}>
       <li style={itemStyles}>
-        <Button primary onClick={action('clicked')}>Normal Button</Button>
+        <Button onClick={action('clicked')}>Normal Button</Button>
+        <pre>{'<Button>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button primary outlined onClick={action('clicked')}>Outlined Button</Button>
+        <pre>{'<Button outlined>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button primary link onClick={action('clicked')}>Link Button</Button>
+        <pre>{'<Button link>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button disabled onClick={action('clicked')}>Disabled Button</Button>
+        <pre>{'<Button disabled>...</Button>'}</pre>
       </li>
     </ul>
   ))
@@ -42,15 +46,19 @@ storiesOf('Button', module)
     <ul style={listStyles}>
       <li style={itemStyles}>
         <Button small primary onClick={action('clicked')}>Small Button</Button>
+        <pre>{'<Button small>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button primary onClick={action('clicked')}>Medium Button</Button>
+        <pre>{'<Button>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button large primary onClick={action('clicked')}>Large Button</Button>
+        <pre>{'<Button large>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button xlarge primary onClick={action('clicked')}>X-Large Button</Button>
+        <pre>{'<Button xlarge>...</Button>'}</pre>
       </li>
     </ul>
   ))
@@ -58,15 +66,19 @@ storiesOf('Button', module)
     <ul style={listStyles}>
       <li style={itemStyles}>
         <Button onClick={action('clicked')}>Default Button</Button>
+        <pre>{'<Button>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button primary onClick={action('clicked')}>Primary Button</Button>
+        <pre>{'<Button primary>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button secondary onClick={action('clicked')}>Secondary Button</Button>
+        <pre>{'<Button secondary>...</Button>'}</pre>
       </li>
       <li style={itemStyles}>
         <Button danger onClick={action('clicked')}>Danger Button</Button>
+        <pre>{'<Button danger>...</Button>'}</pre>
       </li>
     </ul>
   ));
