ae979f5a6bcd81146fcc4b4369e85fcfbace78a1
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2017-03-16 00:00:16 -0700

    [FEATURE] add button component types, colors, and sizes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,24 +2,28 @@ import React from 'react';
 import classNames from 'classnames';
 import './button.css';
 
-const Button = ({
-  children,
-  onClick,
-  small,
-  large,
-  xlarge
-}) => {
+const Button = (props) => {
+  const { onClick } = props; // Actions
+  const { small, large, xlarge } = props; // Sizes
+  const { primary, secondary, danger } = props; // Colors
+  const { outlined, link } = props; // Types
+  const { disabled } = props; // States
+
   const classes = classNames({
-    btn: true,
+    'btn': true,
     'btn--sm': small,
     'btn--lg': large,
-    'btn--xlg': xlarge
+    'btn--xlg': xlarge,
+    'btn--primary': primary,
+    'btn--secondary': secondary,
+    'btn--danger': danger,
+    'btn--outlined': outlined,
+    'btn--link': link
   });
-  console.log('classnames ', classes);
 
   return (
-    <button className={classes} onClick={onClick}>
-      {children}
+    <button className={classes} onClick={onClick} disabled={disabled}>
+      {props.children}
     </button>
   );
 };
--- src/components/Button/button.story.js
+++ src/components/Button/button.story.js
@@ -2,12 +2,71 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Button from './Button';
 
+const PageDecorator = (story) => (
+  <div style={{
+    padding: '10px 20px',
+    textAlign: 'center'
+  }}>
+    { story() }
+  </div>
+);
+const listStyles = {
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  alignItems: 'flex-start',
+  listStyle: 'none'
+};
+const itemStyles = {
+  marginBottom: 10
+};
+
 storiesOf('Button', module)
+  .addDecorator(PageDecorator)
+  .add('Button types', () => (
+    <ul style={listStyles}>
+      <li style={itemStyles}>
+        <Button primary onClick={action('clicked')}>Normal Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button primary outlined onClick={action('clicked')}>Outlined Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button primary link onClick={action('clicked')}>Link Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button disabled onClick={action('clicked')}>Disabled Button</Button>
+      </li>
+    </ul>
+  ))
   .add('Button sizes', () => (
-    <div style={{ 'display': 'flex', 'justify-content': 'space-around', 'align-items': 'center' }}>
-      <div><Button small onClick={action('clicked')}>Small Button</Button></div>
-      <div><Button onClick={action('clicked')}>Medium Button</Button></div>
-      <div><Button large onClick={action('clicked')}>Large Button</Button></div>
-      <div><Button xlarge onClick={action('clicked')}>X-Large Button</Button></div>
-    </div>
+    <ul style={listStyles}>
+      <li style={itemStyles}>
+        <Button small primary onClick={action('clicked')}>Small Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button primary onClick={action('clicked')}>Medium Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button large primary onClick={action('clicked')}>Large Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button xlarge primary onClick={action('clicked')}>X-Large Button</Button>
+      </li>
+    </ul>
+  ))
+  .add('Button colors', () => (
+    <ul style={listStyles}>
+      <li style={itemStyles}>
+        <Button onClick={action('clicked')}>Default Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button primary onClick={action('clicked')}>Primary Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button secondary onClick={action('clicked')}>Secondary Button</Button>
+      </li>
+      <li style={itemStyles}>
+        <Button danger onClick={action('clicked')}>Danger Button</Button>
+      </li>
+    </ul>
   ));
